4d50526bb89cd4d3c55451cab0001b6aeccbc24a
SATYA <SATYA@192.168.0.2>
2019-07-06 22:54:39 +0530

    Developer s Commit 1

---
+++ src/Test02.java
@@ -0,0 +1,13 @@
+
+public class Test02 {
+	public static void main(String[] args) {
+
+		int a = 5;
+
+		if (a == 5) {
+			System.out.println("equal");
+		} else {
+			System.out.println("not equals");
+		}
+	}
+}
