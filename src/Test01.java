8782cb3bf1de29b5bafabea05753825ce3db4309
SATYA <SATYA@192.168.0.4>
2019-07-06 14:11:33 +0530

    TL 1st Commit...

---
+++ src/Test01.java
@@ -0,0 +1,18 @@
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int item=13;
+		if (item<20) {
+			System.out.println("less than 20...");
+		}
+		else if(item<0){
+			System.out.println("negative number...");
+		}
+		else {
+			System.out.println("greater than 20...");
+		}
+
+	}
+
+}
